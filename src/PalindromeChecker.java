367092572bf0fdeb182fc9efbfd7bf9cffb4e174
harshinishori18 <harshinishori15@gmail.com>
2026-02-12 10:25:13 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/PalindromeChecker.java
@@ -0,0 +1,17 @@
+import java.sql.SQLOutput;
+
+public class PalindromeChecker {
+    /*
+    UC1:APPLICATION ENTRY AND WELCOME MESSAGE
+     */
+    public static void main(String[] args){
+        System.out.println("================================");
+        System.out.println("WELCOME TO PALINDROME CHECKER MANAGEMENT SYSTEM");
+        System.out.println("VERSION: 1.0");
+        System.out.println("AUTHOR: HARSHINI SHORI");
+        System.out.println("DESCRIPTION: This App checks whether a given number is a Palindrome or not.");
+        System.out.println("SYSTEM INITIALIZED SUCCESSFULLY");
+        System.out.println("==================================");
+
+    }
+}
