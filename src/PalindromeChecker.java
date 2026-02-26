56a111674d0d733be6be537bc463684f0af377a4
harshinishori18 <harshinishori15@gmail.com>
2026-02-26 09:33:11 +0530

    Harshini Added UC3 String Reverse

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -4,7 +4,7 @@ public class PalindromeChecker {
     /*
     UC2:Print a Hardcoded Palindrome Result
      */
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String input = "madam";
         boolean isPalindrome = true;
 
